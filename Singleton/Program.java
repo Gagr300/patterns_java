0b011d19b526e89788d9954c3028bb8ab7eb897d
Gromova Galina <71606935+Gagr300@users.noreply.github.com>
2023-03-27 01:04:19 +0400

    Update Program.java

--- Singleton/Program.java
+++ Singleton/Program.java
@@ -1,1 +1,18 @@
+public class Program {
+    public static void main(String[] args){
+        LanguageManager languageManager = LanguageManager.getInstance();
+        System.out.println(languageManager);
+        languageManager.setTextLanguage("RUS");
+        languageManager.setAudioLanguage("ENG");
+
+        System.out.println(languageManager.getTextLanguage());
+        System.out.println(languageManager.getAudioLanguage());
+
+        LanguageManager languageManager2 = LanguageManager.getInstance();
+        System.out.println(languageManager2);
+
+        System.out.println(languageManager2.getTextLanguage());
+        System.out.println(languageManager2.getAudioLanguage());
+    }
+}
 
