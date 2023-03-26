6b2fabac88f639910c692487d53231bbfc9fa2a7
Gromova Galina <71606935+Gagr300@users.noreply.github.com>
2023-03-27 00:25:31 +0400

    Create LanguageManager.java

---
+++ LanguageManager.java
@@ -0,0 +1,27 @@
+//Singleton
+public class LanguageManager {
+    private static LanguageManager instance;
+    private static String textLanguage;
+    private static String audioLanguage;
+
+    protected LanguageManager(){}
+
+    public static LanguageManager getInstance() {
+        if (instance == null) {
+            instance = new LanguageManager();
+        }
+        return instance;
+    }
+    public void setTextLang(String textLanguage) {
+        this.textLanguage = textLanguage;
+    }
+    public void setAudioLang(String audioLanguage) {
+        this.audioLanguage = audioLanguage;
+    }
+    public String getTextLanguage() {
+        return textLanguage;
+    }
+    public String getAudioLanguageLang() {
+        return audioLanguage;
+    }
+}
