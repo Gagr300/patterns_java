1820fae77ba15f42c1f4b10a3887a80e88515e10
Gromova Galina <71606935+Gagr300@users.noreply.github.com>
2023-03-27 08:51:50 +0400

    Update Program.java

--- Factory/Program.java
+++ Factory/Program.java
@@ -1,13 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
     public static void main(String[] args) {
         BurgerFactory factory = new BurgerFactory();
- 
+
         Burger brgr = factory.getBurger("Cheeseburger");
-        brgr.perform();
+        brgr.add();
         brgr = factory.getBurger("DoubleCheeseburger");
-        brgr.perform();
-      
-       List<Burger> order = new ArrayList<>();
-       order.add(factory.getBurger("Hamburger"));
+        brgr.add();
+
+        List<Burger> order = new ArrayList<>();
+        order.add(factory.getBurger("Hamburger"));
+        order.add(factory.getBurger("Cheeseburger"));
+        order.add(factory.getBurger("DoubleCheeseburger"));
+        for (Burger t : order) {
+            t.add();
+        }
     }
 }
