2da4a8bdaa445120e39ef50da7e0f943759ad759
Gromova Galina <71606935+Gagr300@users.noreply.github.com>
2023-04-23 21:47:07 +0400

    Update Program.java

--- Decorator/Program.java
+++ Decorator/Program.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
@@ -8,5 +12,34 @@ public class Program {
         System.out.println(Sandwich1.getDescription() + '\t' + Sandwich1.getCost());
         System.out.println(Sandwich2.getDescription() + '\t' + Sandwich2.getCost());
         System.out.println(Sandwich3.getDescription() + '\t' + Sandwich3.getCost());
+
+        Sandwich Sandwich4;
+        Scanner in = new Scanner(System.in);
+        String sndwch;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Username\\Desktop\\test.txt"));
+            sndwch = reader.readLine();
+            switch (sndwch.toLowerCase()){
+                case "regular":
+                    Sandwich4 = new BaseSandwich();
+                    break;
+                case "mushroom":
+                    Sandwich4 = new MushroomSandwich();
+                    break;
+                case "spicy":
+                    Sandwich4 = new SpicySandwich();
+                    break;
+                default:
+                    System.out.println("There is no such sandwich!");
+            }
+        }
+        catch(IOException e1) {
+            e1.printStackTrace();
+        }
+        catch(Exception e) {
+            System.out.println("Unexcepted Exception");
+            e.printStackTrace();
+        }
+
     }
 }
